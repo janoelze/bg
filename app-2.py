b34ba675a3be125b8d3b535c2e55ba26185801c2
Jan Oelze <jan.oelze@native-instruments.de>
2020-05-15 23:29:25 +0200

    migrate to pyqt

--- app-2.py
+++ app-2.py
@@ -1,17 +1,127 @@
 import sys, time, random
+import json, pygame
+import os.path
+import gc
+import glob
+import random
+import soundfile as sf
+
+from time import time, sleep
+from datetime import datetime
+from os import path
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtCore import Qt
 
-from threading import Thread
+from waveform import Waveform
 
-STATE = {
-    'bpm': 120
+from threading import Thread#
+
+# ram stores
+
+_sound_library = {}
+
+# config
+
+CONFIG = {
+    'store_dir': 'data/',
+    'waveforms_dir': 'waveforms/',
+    'samples_dir': 'samples/',
+    'library_dir': 'library/'
 }
+
+# helper
+
+def log(msg):
+    now = datetime.now()
+    print('%s: %s' % (now.strftime("%H:%M:%S"), msg))
+
+def get_state():
+    if path.exists(CONFIG['store_dir'] + '/state.json'):
+        with open(CONFIG['store_dir'] + '/state.json') as json_file:
+            return json.load(json_file)
+    else:
+        return {
+            'bpm': 120,
+            'bars': 1
+        }
+
+def set_state(state):
+    with open(CONFIG['store_dir'] + '/state.json', 'w') as outfile:
+        return json.dump(state, outfile)
+
+def load_samples():
+    count = len(glob.glob(CONFIG['samples_dir'] + '/*.wav'))
+
+    log('loading %s samples' % count)
+
+    for filename in glob.glob(CONFIG['samples_dir'] + '/*.wav'):
+        log('loading %s' % filename)
+
+        lib_filename = filename.replace(CONFIG['samples_dir'], CONFIG['library_dir'])
+
+        if not path.exists(lib_filename):
+            log('converting %s' % filename)
+
+            data, samplerate = sf.read(filename)
+            sf.write(lib_filename, data, 44100, subtype='PCM_16')
+
+    for filename in glob.glob(CONFIG['library_dir'] + '/*.wav'):
+        sound = pygame.mixer.Sound(filename)
+        _sound_library[os.path.basename(filename)] = sound
+
+        wv_filename = filename.replace(
+            CONFIG['library_dir'],
+            CONFIG['waveforms_dir']
+            ).replace('.wav', '.gif')
+
+        if not path.exists(wv_filename):
+            log('creating waveform for %s' % filename)
+
+            waveform = Waveform(filename)
+            waveform.save(wv_filename)
+
+# audio channel init
+
+pygame.mixer.pre_init(44100, -16, 2, 256)
+pygame.mixer.init()
+pygame.init()
+
+CHANNELS = [
+    pygame.mixer.Channel(0),
+    pygame.mixer.Channel(1),
+    pygame.mixer.Channel(2),
+    pygame.mixer.Channel(3),
+    pygame.mixer.Channel(4)
+]
+
+# recover state
+
+STATE = get_state()
+
+STATE['current_step'] = 0
+
+STATE['tracks'] = [
+    # {'channel': 0, 'stutter': True, 'volume': 1, 'sample_id': 'dt_kick_big.wav', 'grid': [0,4,8,12]},
+    {'channel': 0, 'stutter': False, 'volume': 0, 'sample_id': 'dt_hat_bart.wav', 'grid': [0]},
+    # {'channel': 2, 'stutter': True, 'volume': 1, 'sample_id': 'dt_fxloop_120_below.wav', 'grid': [0]},
+    # {'channel': 3, 'stutter': True, 'volume': 1, 'sample_id': 'HiHat 17.wav', 'grid': [1,2,3,4,6,8,10,14]},
+    {'channel': 1, 'stutter': False, 'volume': 0, 'sample_id': 'RK_DT4_Beat_Loop_09_124bpm.wav', 'grid': [0,4,8,12]},
+    {'channel': 2, 'stutter': False, 'volume': 1, 'sample_id': 'RK_DT4_Bass_Loop_24_125bpm_C.wav', 'grid': [0]},
+    {'channel': 3, 'stutter': False, 'volume': 0, 'sample_id': 'Echo Noise Burst 01.wav', 'grid': [0]},
+    # {'channel': 3, 'stutter': True, 'volume': 1, 'sample_id': 'HiHat 17.wav', 'grid': [1,2,3,4,6,8,10,14]},
+]
+
+# print(CHANNELS[0])
+
+set_state(STATE)
+
+# load library
+
+load_samples()
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -19,16 +129,14 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.label = QtWidgets.QLabel()
         canvas = QtGui.QPixmap(400, 300)
+
         self.label.setPixmap(canvas)
         self.setCentralWidget(self.label)
         self.draw_something()
-        # while True:
-        #     self.draw_something()
-            # time.sleep(0.1)
 
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.draw_something)
-        self.timer.start(100)
+        self.timer.start(50)
 
     def draw_something(self):
         painter = QtGui.QPainter(self.label.pixmap())
@@ -36,7 +144,6 @@ class MainWindow(QtWidgets.QMainWindow):
         def drawTracks(painter):
             x = random.randint(0,100)
             y = random.randint(0,100)
-            
 
             width = 40
             y = 0
@@ -47,21 +154,6 @@ class MainWindow(QtWidgets.QMainWindow):
                         QtGui.QImage("waveforms/Abletunes TSD Snare 27.gif")
                     )
                 y = y + width
-
-            # image = QtGui.QImage('waveforms/Abletunes TSD Snare 27.gif')
-            # output = QtGui.QImage(image.size(), QtGui.QImage.Format_ARGB32)
-            # output.fill(QtCore.Qt.transparent)
-            # # painter = QtGui.QPainter(output)
-
-            # points = [(444, 203), (623, 243), (691, 177), (581, 26), (482, 42)]
-            # polygon = QtGui.QPolygonF([QtCore.QPointF(*point) for point in points])
-
-            # path = QtGui.QPainterPath()
-            # path.addPolygon(polygon)
-            # painter.setClipPath(path)
-            # painter.drawImage(QtCore.QPoint(), image)
-            # painter.end()
-            # output.save('out.png')
 
         def drawCursor(painter):
             pen = QPen(Qt.white, 3)
@@ -75,15 +167,81 @@ class MainWindow(QtWidgets.QMainWindow):
         drawTracks(painter)
         drawCursor(painter)
         drawText(painter)
-        
+
         painter.end()
+
         self.update()
 
 def audioManager():
+    def shuffle_track(TRACK):
+        inc = random.randint(1,8)
+        grid = []
+        step = 0
+
+        while step <= max_steps:
+            grid.append(step)
+            step = step + inc
+
+        TRACK['grid'] = grid
+
+    def play(channel, sample_id):
+        if sample_id in _sound_library:
+            channel.play(_sound_library[sample_id])
+
+    DELTA = (60/(STATE['bpm'] * 4))
+    GOAL = time()
+
+    max_steps = 16 * STATE['bars']
+    step = STATE['current_step']
+
     while True:
-        STATE['bpm'] = random.randint(0,1000)
-        print('a')
-        time.sleep(1)
+        if step >= max_steps:
+            step = 0
+
+        STATE['current_step'] = step
+
+        play_output = []
+
+        for TRACK in STATE['tracks']:
+            channel = CHANNELS[TRACK['channel']]
+            sample_id = TRACK['sample_id']
+
+            if 'volume' in TRACK:
+                channel.set_volume(TRACK['volume'])
+
+            if 'stutter' in TRACK and TRACK['stutter']:
+                channel.set_volume(random.randint(80,100)/100)
+
+            if 'sample_id' in TRACK and 'grid' in TRACK and TRACK['sample_id']:
+                if step in TRACK['grid']:
+                    if channel.get_busy():
+                        channel.stop()
+
+                    play(channel, TRACK['sample_id'])
+                    play_output.append('[%s]' % TRACK['sample_id'][0:5])
+
+        log('playing %s' % (' '.join(play_output)))
+
+        # print('%s/%s %s' % (step, max_steps, ))
+
+        if step % 8 == 0 and random.randint(0,100) > 40:
+            random_track = random.choice(STATE['tracks'])
+            shuffle_track(random_track)
+
+        step = step + 1
+
+        
+
+        GOAL += DELTA
+
+        gc.collect()
+
+        sleepInterval = GOAL - time()
+
+        if sleepInterval >= 0:
+            sleep(sleepInterval)
+        else:
+            sleep(0)
 
 if __name__ == '__main__':
     Thread(target = audioManager).start()
